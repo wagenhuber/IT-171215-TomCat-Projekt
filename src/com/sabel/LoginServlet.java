f21fcd75f2da8086acb8c0ba4a26aa26ab18e72d
wagenhuber <wagenhuber@gmx.net>
2017-12-15 08:56:59 +0100

    Step2: Adding a request parameter

--- src/com/sabel/LoginServlet.java
+++ src/com/sabel/LoginServlet.java
@@ -15,16 +15,9 @@ public class LoginServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
-        writer.println("<html>");
-        writer.println("<head>");
-        writer.println("<title>My first WebApp</title>");
-        writer.println("</head>");
-        writer.println("<body>");
-        writer.println("<h1>Meine erste WebApp</h1>");
-        writer.println("<p>Mein erster Paragraph<p>");
-        writer.println("<p>Hallo I3A<p>");
-        writer.println("</body>");
+        //req ist das angefragte Objekt
+        req.setAttribute("name", req.getParameter("name"));
+        req.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(req, resp);
 
 
     }
---
+++ web/WEB-INF/views/login.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: wagenhuberg
+  Date: 15.12.2017
+  Time: 08:42
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Meine erste JSP</title>
+</head>
+<body>
+Meine erste JSP!
+Meine Name ist: ${name}
+</body>
+</html>
